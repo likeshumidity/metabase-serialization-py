a5ca39b9285599d8d821416098b5c4bb3a479b3e
Jacob Joseph <jacob@dayflower.io>
2024-06-06 09:58:10 -0400

    created change_requests module

--- src/metabase_serialization_py/__init__.py
+++ src/metabase_serialization_py/__init__.py
@@ -3,36 +3,17 @@ import logging
 import os
 import hashlib
 
-from yaml.constructor import ConstructorError as ConstructorError_yaml
-
 from metabase_serialization_py.metabase_export import MetabaseExport
-from metabase_serialization_py.yaml import parse_yaml
+from metabase_serialization_py.change_requests import ChangeRequests
 
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
-# TODO: add CLI parameter for logging level
 
 TIMESTAMP = datetime.now().isoformat().replace('-', '').replace('.', '').replace('T', '').replace(':', '')
 
-
 # TODOs
 # TODO: replace references to "member" with something more clear like "archive member" or "exported object"
-
-
-def create_change_list(change_list_file_path, metabase_export):
-    """Creates list of changes based on change_list_file and dependencies."""
-    change_list = ()
-
-    with open (change_list_file_path, 'r') as change_list_file:
-        change_list = parse_yaml(change_list_file)
-
-    # TODO: for each change in change list
-    # TODO: traverse tree of change and dependent changes
-    # TODO: validate that related entity keys exist
-
-    import pdb; pdb.set_trace();  # TODO: REMOVE after debugging
-
-    return change_list
+# TODO: add CLI parameter for logging level
 
 
 def cli(export_path, change_list_file_path, output_path=os.getcwd()):
@@ -54,8 +35,8 @@ def cli(export_path, change_list_file_path, output_path=os.getcwd()):
 
     metabase_export = MetabaseExport(export_path)
 
-    change_list = create_change_list(change_list_file_path, metabase_export)
+    change_requests = ChangeRequests(change_list_file_path, metabase_export)
 
     import pdb; pdb.set_trace();  # TODO: REMOVE after debugging
 
-    # process_changes(metabse_export, change_list, output_path)
+    # process_changes(metabse_export, change_requests, output_path)
---
+++ src/metabase_serialization_py/change_requests.py
@@ -0,0 +1,54 @@
+"""Change List helper for Metabase Serialization."""
+
+# from yaml.constructor import ConstructorError as ConstructorError_yaml
+
+from metabase_serialization_py.yaml import parse_yaml
+
+
+# TODOs
+# TODO: handle create card
+# TODO: handle create model
+# TODO: handle create dashboard
+# TODO: handle create collection
+# TODO: handle archive collection contents before update
+# TODO: handle replace card
+# TODO: handle replace model
+# TODO: handle replace dashboard
+# TODO: handle replace collection
+# TODO: handle update database
+# TODO: handle update schema
+# TODO: handle update table
+# TODO: handle update field
+# TODO: handle update card
+# TODO: handle update model
+# TODO: handle update dashboard
+
+
+class ChangeRequests:
+    change_requests = None
+
+    def __init__(self, change_list_file_path, metabase_export):
+        """Creates list of changes based on change_list_file and dependencies."""
+        with open (change_list_file_path, 'r') as change_list_file:
+            self.change_requests = parse_yaml(change_list_file)
+
+        self.validate_change_requests(metabase_export)
+
+        self.sort_change_requests_by_precedence()
+
+        import pdb; pdb.set_trace();  # TODO: REMOVE after debugging
+
+        return change_list
+
+    def sort_change_requests_by_precedence(self):
+        """Sorts change requests by order of precendence to avoid dependency issues."""
+
+        import pdb; pdb.set_trace();  # TODO: REMOVE after debugging
+
+    def validate_change_requests(self, metabase_export):
+        """Validates change requests against Metabase Export."""
+
+        for i, change_request in enumerate(self.change_requests):
+            # TODO: traverse tree of change and dependent changes
+            # TODO: validate that specified entity keys and path references exist
+            import pdb; pdb.set_trace();  # TODO: REMOVE after debugging
