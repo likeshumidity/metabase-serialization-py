8ac20ef9b60d1bbcf8431fea05c8e4af17222866
Jacob Joseph <jacob@dayflower.io>
2024-06-06 07:01:09 -0400

    check parameter existence

--- src/metabase_serialization_py/__init__.py
+++ src/metabase_serialization_py/__init__.py
@@ -37,13 +37,19 @@ def create_change_list(change_list_file_path, metabase_export):
 def cli(export_path, change_list_file_path, output_path=os.getcwd()):
     """Metabase Serialization CLI entry point."""
 
-    LOGGER.info(f'.. Export Path: {export_path}')
-    LOGGER.info(f'.. Change List Path: {change_list_file_path}')
-    LOGGER.info(f'.. Output Path: {output_path}')
+    PARAMETERS = (
+        ('Export Path', export_path, 'EXPORT_PATH argument', os.path.isfile, ),
+        ('Change List Path', change_list_file_path, 'CHANGE_LIST_FILE_PATH argument', os.path.isfile, ),
+        ('Output Path', output_path, '--output flag', os.path.isdir, ),
+    )
 
-    if not os.path.isdir(output_path):
-        LOGGER.error(f'Specified output path not found: "{output_path}". Please create the directory or update the --output flag.')
-        exit(1)
+    for param_title, param_value, param_instructions, param_test in PARAMETERS:
+        LOGGER.info(f'{param_title}: {param_value}')
+
+        if not param_test(param_value):
+            LOGGER.error(f'.. Specified {param_title.lower()} not found: "{param_value}". Review the parameter for the {param_instructions}.')
+            exit(1)
+
 
     metabase_export = MetabaseExport(export_path)
 
